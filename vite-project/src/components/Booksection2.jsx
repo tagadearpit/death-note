ab00c1e18572f39df0b422fe5a27d1d58f3fd398
Arpit Tagade <143588157+tagadearpit@users.noreply.github.com>
2026-06-22 15:27:44 +0530

    fix: correct CSS import casing in Booksection2.jsx

--- vite-project/src/components/Booksection2.jsx
+++ vite-project/src/components/Booksection2.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import './BookSection2.css'
+import './Booksection2.css'
 
 gsap.registerPlugin(ScrollTrigger)
 
